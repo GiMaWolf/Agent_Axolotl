0460972ddfb3f5e836a87a15ecd806c78ccf485b
Felix Kalley <felix.kalley@gmail.com>
2018-09-28 16:31:36 +0200

    init partview
    whatever

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SelectViewComponent } from './select-view/select-view.component';
+import { PartViewComponent } from './part-view/part-view.component';
 
 const routes: Routes = [
     {
@@ -14,6 +15,10 @@ const routes: Routes = [
     {
         path: 'selectView',
         component: SelectViewComponent
+    },
+    {
+        path: 'partView',
+        component: PartViewComponent
     }
 ];
 
