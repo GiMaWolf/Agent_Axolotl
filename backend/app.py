b6ea59942ffaba0c3a9c1d6ca894f78c543d159b
Markus Guder <progamer101101@gmx.de>
2018-09-28 13:10:15 +0200

    Finished app.py

--- backend/app.py
+++ backend/app.py
@@ -2,29 +2,35 @@ from flask import Flask, request, redirect, url_for
 from threading import Timer
 from flask.ext.cors import CORS
 
-from RunningDebates import RunningDebates
-
+from MainViewConvert import MainViewConvert
+from SelectViewConvert import SelectViewConvert
+from PartViewConvert import PartViewConvert
+from CompareViewConvert import CompareViewConvert
 
 app = Flask(__name__)
 CORS(app)
-runDebates = RunningDebates()
+
+mainView = MainViewConvert()
+selectView = SelectViewConvert()
+partView = PartViewConvert()
+compareView = CompareViewConvert()
 
 @app.route('/fleet', methods=['GET'])
 def get_fleet_for_main_view():
-    return "/fleet"
+    return mainView.main_Method()
 
 @app.route('/components', methods=['GET'])
 def get_components_for_selected_view():
-    return "/getComponentsForSelectedView"
+    return selectView.main_Method()
 
 @app.route('/components/<string:component>', methods=['GET'])
 def get_single_component_for_part_view(component):
-    return "/getSingleComponentForPartView"
+    return partView.main_Method(component)
 
 @app.route('/components/<string:component>/details', methods=['GET'])
 def get_single_component_details_for_component_view(component):
-    cars = request.args.get('vehicles');
-    return "/getSingleComponentDetailsForComponentView"
+    cars = [x.strip() for x in request.args.get('vehicles').split(',')]
+    return compareView.main_Method(component, cars)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
