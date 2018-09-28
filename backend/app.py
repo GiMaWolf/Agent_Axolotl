c45d98ff777a7e6111d5350e31ed1e43e2d628b6
Markus Guder <progamer101101@gmx.de>
2018-09-28 16:36:10 +0200

    finised backend compareView

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from flask.ext.cors import CORS
 #from MainViewConvert import MainViewConvert
 #from SelectViewConvert import SelectViewConvert
 from scipts.partViewConvert import PartViewConvert
-#from CompareViewConvert import CompareViewConvert
+from scipts.compareViewConvert import CompareViewConvert
 
 app = Flask(__name__)
 CORS(app)
@@ -13,7 +13,7 @@ CORS(app)
 #mainView = MainViewConvert()
 #selectView = SelectViewConvert()
 partView = PartViewConvert()
-#compareView = CompareViewConvert()
+compareView = CompareViewConvert()
 
 @app.route('/fleet', methods=['GET'])
 def get_fleet_for_main_view():
@@ -32,9 +32,12 @@ def get_single_component_for_part_view(component):
 
 @app.route('/components/<string:component>/details', methods=['GET'])
 def get_single_component_details_for_component_view(component):
-    #cars = [x.strip() for x in request.args.get('vehicles').split(',')]
-    #return compareView.main_method(component, cars)
-    return ""
+    cars = [x.strip() for x in request.args.get('vehicles').split(',')]
+    try:
+        results = compareView.main_method(component, cars)
+    except Exception as e:
+        print(e)
+    return results
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
